9d82215e6f36245957328cca5ef878e6135c3552
WHEOfficial <46579241+WHEOfficial@users.noreply.github.com>
2023-04-28 14:05:53 -0400

    Finished day 9

--- day09/basins.py
+++ day09/basins.py
@@ -1,6 +1,27 @@
+import math
+
+def basin(coord,  heightmap):
+    x, y = coord[1], coord[0]
+    if heightmap[y][x] == 9:
+        return 0
+
+    total = 1
+    heightmap[y][x] = 9
+    if x > 0:
+        total += basin([y, x-1], heightmap)
+    if x < len(heightmap[0]) - 1:
+        total += basin([y, x+1], heightmap)
+    if y > 0:
+        total += basin([y-1, x], heightmap)
+    if y < len(heightmap) - 1:
+        total += basin([y+1, x], heightmap)
+    
+    return total
+
 with open("input.txt") as infile:
     heightmap = infile.read().splitlines()
     low_points = []
+    basins = []
     for i, row in enumerate(heightmap):
         col_list = []
         for col in row:
@@ -21,6 +42,10 @@ with open("input.txt") as infile:
             if y < len(heightmap) - 1:
                 if heightmap[y+1][x] <= col:
                     continue
-            low_points.append(col)
+            low_points.append([y, x])
     
-    print(sum(low_points) + len(low_points))
+    for low_point in low_points:
+        basins.append(basin(low_point, heightmap))
+    
+    product = math.prod(sorted(basins)[-3:])
+    print(product)
